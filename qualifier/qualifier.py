c8365ed70e54ed9f1d6c919cf4d0babfd4381c7c
Harshit Daga <78150691+harshitdaga7@users.noreply.github.com>
2022-07-16 17:57:13 +0530

    qualifier solution

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -1,7 +1,6 @@
 import typing
 from dataclasses import dataclass
 
-
 @dataclass(frozen=True)
 class Request:
     scope: typing.Mapping[str, typing.Any]
@@ -31,4 +30,38 @@ class RestaurantManager:
             Request object containing information about the sent
             request to your application.
         """
-        ...
+        # print('printing requests')
+        # print(request.scope)
+        # print(request.scope["id"])
+
+        if(request.scope["type"] == "staff.onduty"):
+            
+            self.staff[request.scope["id"]] = request
+            #print(self.staff)
+        elif(request.scope["type"] == "staff.offduty"):
+            
+            del self.staff[request.scope["id"]]
+            #print(self.staff)
+
+        elif(request.scope["type"] == "order"):
+            
+            required_speciality = request.scope['speciality']
+
+            #finding speciality
+
+            for c in self.staff:
+
+                if required_speciality in self.staff[c].scope["speciality"]:
+                    found = self.staff[c]
+                    full_order =  await request.receive()
+                    await found.send(full_order)
+
+                    result = await found.receive()
+                    await request.send(result)
+                    break
+            
+                
+        else:
+            pass
+            # print("unidentified type")
+            # print(request.scope)
